c0c28bd13d1df76a0fa05ea703d5ddf2d5e9ee83
Ayush <ayush.varma@rearc.io>
2020-07-27 11:33:50 -0500

    Fix url_end

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -10,7 +10,7 @@ def data_to_s3(frmt):
     # otherwise downloads and uploads to s3
 
     source_dataset_url = 'https://fred.stlouisfed.org/graph/fredgraph'
-    url_end = '?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=HOUST&scale=left&cosd=1959-01-01&coed=2020-06-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2020-07-27&revision_date=2020-07-27&nd=1959-01-01'
+    url_end = '?id=HOUST'
 
     try:
         response = urlopen(source_dataset_url + frmt + url_end)
